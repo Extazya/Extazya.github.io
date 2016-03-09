4cf551a36652c2bbefeede4dabe83f456db3c2a2
Mathias DEZITTER <mdezitte@e1r9p10.42.fr>
2016-03-09 16:06:21 +0100

    lol

---
+++ index.php
@@ -0,0 +1,64 @@
+<HTML>
+<head>
+	<title>Mathias Dezitter</title>
+	<link rel="stylesheet" href="page.css" />
+</head>
+<body>
+<div class="top">
+	<div class="titre_div">
+		<HR class="sep"></HR>
+		<p class="wtitre"><g>Dezitter Mathias</g></p>
+		<p class="wtitre_sub">Developeur</p>
+		<HR class="sep"></HR>
+	</div>
+	<div class="content">
+		<p class="wtitre_sub">Langage C</p>
+		<p class="listing">- Developpement d'un shell type Bash au complet.</p>
+		<p class="listing">- Recode de la commande LS avec flag.</p>
+		<p class="listing">- Developpement d'un programme de selection a l'aide des Termcaps.</p>
+		<p class="listing">- Recode de printf (conversions : sSpdDioOuUxXcC).</p>
+		<p class="listing">- Developpement d'un programme ayant pour but les differents type de tri existant.</p>
+		<p class="listing">- Developpement d'un programme utilisant le brute force pour resoudre une problematique.</p>
+		<p class="listing">- Developpement d'une IA de puissance 4 utilisant l'algorithme de Minimax (Negamax).</p>
+		<p class="listing">- Recode du jeu 2048.</p>
+		<p class="listing">- Developpement d'une representation en vue isometrique de differente forme.</p>
+		<p class="listing">- Developpement d'une representation du plusieurs fractal (Julia, Mandelbrot, Burning ship...).</p>
+		<p class="listing">- Developpement d'un jeu de labyrinthe 3D avec la technique de raycast.</p>
+		<p class="listing">- Creation d'une librairie personnel.</p>
+		<br/>
+		<HR class="sep"></HR>
+		<p class="wtitre_sub">Unity et C#</p>
+		<p class="listing">- Recode de jeu simple (Pong, flappy bird)</p>
+		<p class="listing">- Recode d'un Hotline Miami</p>
+		<p class="listing">- Developpement d'un debut de jeu type Hack'n slash</p>
+		<p class="listing">- Developpement de divers petit jeu, utilisant la 3D dans le cadre d'apprentissage</p>
+		<br/>
+		<HR class="sep"></HR>
+		<p class="wtitre_sub">Programation objet</p>
+		<p class="listing">- Developpement d'un jeu type Space invader en C++</p>
+		<p class="listing">- Developpement d'un outils de gestion des taches et perfomance informatique en C++ et QT</p>
+		<p class="listing">- Developpement d'un programme de trajet de metro (algorithme de Dijkstra) en Python</p>
+		<br/>
+		<HR class="sep"></HR>
+		<p class="wtitre_sub">Securiter et Assembleur</p>
+		<p class="listing">- Creation d'une librairie personnel en assembleur utilisable par le C</p>
+		<p class="listing">- Resolution de divers challenge de securiter (systeme, cracking) sur le site root-me</p>
+		<br/>
+		<HR class="sep"></HR>
+		<p class="wtitre_sub">Web et application</p>
+		<p class="listing">- Developpement d'une application de messagerie / contact pour android (JAVA)</p>
+		<p class="listing">- Developpement d'un site de E-commerce en local pour apprendre le systeme MVC sous Symfony2</p>
+		<p class="listing">- Developpement d'un site de montage avec interraction de la webcam sans framework en PHP</p>
+		<HR class="sep"></HR>
+		<br/>
+		<br/>
+		<p class="listing">Pour me contacter : mdezitte@student.42.fr ou 0645555121.</p>
+		<br/>
+		<br/>
+	</div>
+</div>
+</body>
+<footer>
+	
+</footer>
+</HTML>
